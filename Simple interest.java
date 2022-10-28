c5681dcf814e5c8c88173128769c1cc20d880347
AF0252046 <116809915+AF0252046@users.noreply.github.com>
2022-10-28 23:09:55 +0530

    Update and rename Simple_interest.java to Simple interest.java

--- Simple interest.java
+++ Simple interest.java
@@ -2,7 +2,7 @@
 public class Simple_interest {
 
 	public static void main(String[] args) {
-		float p, r,  t,  si; // principal amount, rate, time and simple interest respectively  
+		float p, r,  t,  si;
         p = 12;  r = 10; t = 2;  
          si  = (p*r*t)/100;   
         System.out.println("Simple Interest is: " +si);  
